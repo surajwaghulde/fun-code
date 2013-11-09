6155237beedbc3221801f9ac2e4aec4b0527528c
Suraj Waghulde <surajwaghulde@gmail.com>
2013-11-09 11:00:51 -0800

    wrote card instance for creating deck

--- BlackJack/Card.java
+++ BlackJack/Card.java
@@ -2,4 +2,29 @@ package BlackJack;
 
 public class Card {
 
+	public enum Rank {
+		TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE;
+	}
+	
+	public enum Suit {
+		SPADES, HEARTS, CLUBS, DIAMONDS;
+	}
+	
+	private Rank rank;
+	private Suit suit;
+	
+	public Card(Rank rank, Suit suit) {
+		super();
+		this.rank = rank;
+		this.suit = suit;		
+	}
+	
+	public boolean equals(Card card) {
+		return (this.rank == card.rank) && (this.suit == card.suit);
+	}
+	
+	public String toString() {
+		return rank + " " + suit;
+	}
+	
 }
